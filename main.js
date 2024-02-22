2400123e85208a875e428a4853e25af9981c4a86
janani-2047 <116658619+janani-2047@users.noreply.github.com>
2024-02-22 16:07:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,136 @@
+/*=============== SHOW MENU ===============*/
+if (typeof window !== "undefined") {
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+     
+
+
+if(navToggle){
+    navToggle.addEventListener('click',() =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click',() =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50)header.classList.add('scroll-header'); 
+    else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/*=============== POPULAR SWIPER ===============*/
+let swiperPopular = new Swiper(".popular_container", {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+    grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      spaceBetween: 48,
+    },
+  },
+});
+
+
+
+/*=============== MIXITUP FILTER FEATURED ===============*/
+let mixerFeatured = mixitup('.featured_content', {
+    selectors: {
+        target: '.featured_card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+
+/* Link active featured */ 
+const linkFeatured = document.querySelectorAll('.featured_item')
+
+function activeFeatured(){
+    linkFeatured.forEach(l=> l.classList.remove('active-featured'))
+    this.classList.add('active-featured')
+}
+linkFeatured.forEach(l=> l.addEventListener('click', activeFeatured))
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show_scroll')
+}
+window.addEventListener('scroll',scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }
+        else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll',scrollActive)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+     // reset: true
+})
+
+sr.reveal('.home_title, .popular_container, .features_img, .featured_filters')
+sr.reveal('.home_subtitle', {delay: 500})
+sr.reveal('.home_elec', {delay: 600})
+sr.reveal('.home_img', {delay: 800})
+sr.reveal('.home_car-data', {delay: 900, interval: 100, origin: 'bottom'})
+sr.reveal('.home_button', {delay: 1000, origin: 'bottom'})
+sr.reveal('.about_group, .offer_data', {origin: 'left'})
+sr.reveal('.about_data, .offer_img', {origin: 'right'})
+sr.reveal('.features_map', {delay: 600, origin: 'bottom'})
+sr.reveal('.features_card', {interval: 300})
+sr.reveal('.featured_card, .logos_content, .footer_content', {interval: 100})
+
+
+
+
+}
